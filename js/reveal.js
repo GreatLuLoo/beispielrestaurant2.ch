2dd6b9891768ca1bcee3af8f0001fecfecf4f833
Lenny Uhlir <Lenny.uhlir@icloud.com>
2026-01-05 20:32:20 +0100

    Update reveal.js

--- js/reveal.js
+++ js/reveal.js
@@ -2,10 +2,11 @@ const reveals = document.querySelectorAll(".reveal");
 
 const revealOnScroll = () => {
   const trigger = window.innerHeight * 0.85;
-
   reveals.forEach(el => {
     const top = el.getBoundingClientRect().top;
-    if (top < trigger) el.classList.add("active");
+    if(top < trigger){
+      el.classList.add("active");
+    }
   });
 };
 
